c12c6aedcd429d5b33119353ee00878e19df7276
Thales Henrique <thenrique@bvp.com>
2025-02-19 16:48:33 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.send_email import get_counts, generate_email_template, send_email
+
+def main():
+    counts = get_counts()
+    print("Contagens obtidas:", counts)
+
+    email_html = generate_email_template(counts)
+
+    recipient_email = "thenrique@bvp.com"  
+
+    send_email(recipient_email, email_html)
+
+if __name__ == "__main__":
+    main()
